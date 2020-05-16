52489d73b004c484220b580d439580a7999fa8ca
Justin Kichler <jkichler@gmail.com>
2020-05-15 22:55:04 -0400

    'basic structure set up'

---
+++ InventoryTree.js
@@ -0,0 +1,151 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.parent = null;
+    this.children = [];
+    this.items = [];
+    this.prev = null;
+    this.next = null;
+  }
+  addChild(node) {
+    node.parent = this;
+    this.children.push(node);
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(newNode) {
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    const poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+  shift() {
+    if (this.length === 0) return undefined;
+    const oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  unshift(newNode) {
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count, current;
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode !== null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+
+    const newNode = new Node(val);
+    const beforeNode = this.get(index - 1);
+    const afterNode = beforeNode.next;
+
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++;
+    return true;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  getNode(search) {
+    let node = this.root;
+    const queue = new DoublyLinkedList();
+    if (node.value === search) return node;
+    if (node.children.length) {
+      node.children.forEach((el) => {
+        queue.push(el);
+      });
+    }
+    while (queue.length) {
+      node = queue.shift();
+      if (node.value === search) return node;
+      if (node.children.length) {
+        node.children.forEach((el) => {
+          queue.push(el);
+        });
+      }
+    }
+    return false;
+  }
+}
+
+const test = new Tree();
+test.root = new Node('cat');
+test.root.addChild(new Node('tag1'));
+test.root.addChild(new Node('tag2'));
+
+console.log(test.getNode('tag2'));
